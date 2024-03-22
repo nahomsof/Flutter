b1614a8996bcbdc1d2625bace898ceb6f9ff170c
nahomsof <nahomsileshi21@gmail.com>
2024-03-22 11:36:08 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ class App0301 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Material(
-        child: Text("Hello world!",
-            textScaleFactor: 2.0, style: TextStyle(fontSize: 28.0)),
+        child: Text("Hello world!", textScaleFactor: 2.0),
       ),
     );
   }
