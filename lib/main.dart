5244aa42848f59675305cd1eb47ad282be75caa8
nahomsof <nahomsileshi21@gmail.com>
2024-03-22 21:38:44 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ main() => runApp(App0301());
 class App0301 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Hello world!", textScaleFactor: 2.0),
-        ),
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Adding Widgets"),
+          ),
+          body: const Column(
+            children: [
+              Text(
+                "Hello world!",
+                textScaleFactor: 2.0,
+              ),
+              Text("It's lonely for me inside this phone."),
+              Text("That must be taugh")
+            ],
+          )),
     );
   }
 }
