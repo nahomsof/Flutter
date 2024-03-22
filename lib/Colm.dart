45658b34924bf8f7477858889763cafb21f186bb
nahomsof <nahomsileshi21@gmail.com>
2024-03-22 21:38:28 +0300

    commit

---
+++ lib/Colm.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(App0307());
+
+class App0307 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Adding Widgets"),
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Hello world!",
+                    textScaleFactor: 2.0,
+                  ),
+                  Text("It's lonely for me inside this phone.")
+                ],
+              ),
+            )));
+  }
+}
