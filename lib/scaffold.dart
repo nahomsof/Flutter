11785327f4fb919367bddaae631635427fa3b70c
nahomsof <nahomsileshi21@gmail.com>
2024-03-22 17:59:46 +0300

    commit

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-main() => runApp(App0303());
+main() => runApp(const App0303());
 
 class App0303 extends StatelessWidget {
+  const App0303({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text("My First Scaffold"),
-            ),
-            body: Center(
+                title: const Text("My First Scaffold"),
+                elevation: 100,
+                systemOverlayStyle: SystemUiOverlayStyle.dark),
+            body: const Center(
               child: Text("Hello world!"),
             ),
-            drawer: Drawer(
+            drawer: const Drawer(
               child: Center(
-                child: Text("I'm a drawer."),
+                child: Text("This is drawer"),
               ),
             )));
   }
