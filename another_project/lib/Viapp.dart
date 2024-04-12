100b756d961c90124b9a3b775aa6e0c2b80a0361
nahomsof <nahomsileshi21@gmail.com>
2024-04-12 11:05:12 +0300

    commit

--- another_project/lib/Viapp.dart
+++ another_project/lib/Viapp.dart
@@ -1,9 +1,29 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Myapp())
+void main() {
+  runApp(Firstapp());
 }
 
-class Myapp extends StatelessWidget {
-  
+class Firstapp extends StatelessWidget {
+  const Firstapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: const Color.fromARGB(255, 14, 8, 24),
+            body: Center(
+              child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(70)),
+                  padding: EdgeInsets.all(25),
+                  child: Icon(
+                    Icons.accessibility_rounded,
+                    color: const Color.fromARGB(255, 51, 47, 47),
+                    size: 67,
+                  )),
+            )));
 }
--- another_project/lib/counter_page.dart
+++ another_project/lib/counter_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/count.dart';
+import 'package:count.dart';
 
 class CounterPage extends StatefulWidget {
   _CounterPageState createState() => _CounterPageState();
