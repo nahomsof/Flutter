12c93314cf6002019a3eca9528f4ac001f7566c0
nahomsof <nahomsileshi21@gmail.com>
2024-04-21 12:28:04 +0300

    commit

--- another_project/lib/Todo_App/home_page.dart
+++ another_project/lib/Todo_App/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[200],
+      appBar: AppBar(),
     );
   }
 }
