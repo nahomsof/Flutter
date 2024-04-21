931ad5d695fd350d375991cd20b29b90cb472a03
nahomsof <nahomsileshi21@gmail.com>
2024-04-21 16:13:29 +0300

    commit

---
+++ another_project/lib/Todo_App/main2.dart
@@ -0,0 +1,19 @@
+import 'package:another_project/Todo_App/home_page.dart';
+import 'package:another_project/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
