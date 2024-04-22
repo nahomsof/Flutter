7ee51986e47f7400a982b4f2cc793db0c366f22c
nahomsof <nahomsileshi21@gmail.com>
2024-04-22 21:46:33 +0300

    commit

--- sniker_tute/lib/pages/intro_pages.dart
+++ sniker_tute/lib/pages/intro_pages.dart
@@ -7,31 +7,46 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Image.asset(
-              'lib/Images/logo.png',
-              height: 240,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Image.asset(
+                'lib/Images/logo.png',
+                height: 240,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 48,
-          ),
-          Text(
-            'Just Do It',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Text(
-            'Brand new sneakers and custom kicks made with premium quility',
-            style: TextStyle(fontSize: 16, color: Colors.grey),
-            textAlign: TextAlign.center,
-          )
-        ],
+            const SizedBox(
+              height: 48,
+            ),
+            Text(
+              'Just Do It',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              'Brand new sneakers and custom kicks made with premium quility',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.grey[900]),
+              padding: const EdgeInsets.all(25),
+              child: Center(
+                  child: const Text(
+                'Shop Now',
+                style: TextStyle(color: Colors.white),
+              )),
+            )
+          ],
+        ),
       ),
     );
   }
