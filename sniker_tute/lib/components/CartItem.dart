60e0188270f0be7149380adcaa312e06a52f9db3
nahomsof <nahomsileshi21@gmail.com>
2024-04-26 21:36:10 +0300

    commit

--- sniker_tute/lib/components/CartItem.dart
+++ sniker_tute/lib/components/CartItem.dart
@@ -12,10 +12,15 @@ class CartItem extends StatefulWidget {
 class _CartItemState extends State<CartItem> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(widget.shoe.imagePath),
-      title: Text(widget.shoe.name),
-      subtitle: Text(widget.shoe.price),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[100], borderRadius: BorderRadius.circular(8)),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: Image.asset(widget.shoe.imagePath),
+        title: Text(widget.shoe.name),
+        subtitle: Text(widget.shoe.price),
+      ),
     );
   }
 }
