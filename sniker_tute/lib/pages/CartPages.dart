98eeef792a5335106dd46c4e02ca019f2f976c36
nahomsof <nahomsileshi21@gmail.com>
2024-04-26 21:28:06 +0300

    Commit

--- sniker_tute/lib/pages/CartPages.dart
+++ sniker_tute/lib/pages/CartPages.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sniker_tute/components/CartItem.dart';
+import 'package:sniker_tute/model/cart.dart';
+import 'package:sniker_tute/model/shoe.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,8 +14,29 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Cart"),
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  Text(
+                    "My Cart",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                      child: ListView.builder(itemBuilder: (context, index) {
+                    //get individual shoe
+                    Shoe individualshoe = value.getUserCart()[index];
+                    return CartItem(
+                      shoe: individualshoe,
+                    );
+                    //
+                  }))
+                ],
+              ),
+            ));
   }
 }
