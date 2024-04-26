1d7c3d6597b9d47dce3874b25533b426d039d07a
nahomsof <nahomsileshi21@gmail.com>
2024-04-26 18:48:36 +0300

    commit

--- sniker_tute/lib/pages/ShopPage.dart
+++ sniker_tute/lib/pages/ShopPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sniker_tute/components/Shoetile.dart';
+import 'package:sniker_tute/model/cart.dart';
 
 import '../model/shoe.dart';
 
@@ -13,80 +15,78 @@ class Shoppage extends StatefulWidget {
 class _ShoppageState extends State<Shoppage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(
-              color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Search",
-                style: TextStyle(color: Colors.grey),
-              ),
-              Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-            ],
-          ),
-        ),
-        //Messages
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25.0),
-          child: Text(
-            "everyone flies... Some flies longer than others",
-            style: TextStyle(color: Colors.grey[600]),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                "Hot picks 🔥",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+    return Consumer<Cart>(
+        builder: (context, value, child) => Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(12),
+                  margin: const EdgeInsets.symmetric(horizontal: 25),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Search",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Text(
-                "See all",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Expanded(
-            child: ListView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  Shoe shoe = Shoe(
-                      name: "Air force 1 Low",
-                      description: "Cool Shoe",
-                      imagePath: 'lib/Images/nike-air-force-1-low-.png',
-                      price: "110-140");
-                  return ShoeTile(
-                    shoe: shoe,
-                  );
-                })),
-        Padding(
-          padding: const EdgeInsets.only(top: 25, right: 25, left: 25),
-          child: Divider(
-            color: Colors.white,
-          ),
-        )
-      ],
-    );
+                //Messages
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25.0),
+                  child: Text(
+                    "everyone flies... Some flies longer than others",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hot picks 🔥",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                      ),
+                      Text(
+                        "See all",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: 4,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          Shoe shoe = value.getUsershoe()[index];
+                          return ShoeTile(
+                            shoe: shoe,
+                          );
+                        })),
+                Padding(
+                  padding: const EdgeInsets.only(top: 25, right: 25, left: 25),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ));
   }
 }
