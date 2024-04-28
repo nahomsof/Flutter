5ce2dbec62bad50b427f8fd682130ca48a2d864f
nahomsof <nahomsileshi21@gmail.com>
2024-04-28 14:46:05 +0300

    commit

--- ecommerce_app/lib/Pages/cart_page.dart
+++ ecommerce_app/lib/Pages/cart_page.dart
@@ -1,10 +1,32 @@
+import 'package:ecommerce_app/models/product.dart';
+import 'package:ecommerce_app/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
+  void removeItemFromCart(BuildContext context, Product product) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Remove this item to your cart"),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+          MaterialButton(
+            onPressed: () => context.read<Shop>().addToCart(product),
+            child: Text("Yes"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<Shop>().cart;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -12,6 +34,28 @@ class CartPage extends StatelessWidget {
         elevation: 0,
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Cart Page"),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: cart.length,
+                  itemBuilder: (context, index) {
+                    final item = cart[index];
+
+                    return ListTile(
+                      title: Text(
+                        item.name,
+                      ),
+                      subtitle: Text(item.price.toString()),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.remove),
+                        onPressed: () => removeItemFromCart(context, item),
+                      ),
+                    );
+                  }))
+        ],
       ),
     );
   }
