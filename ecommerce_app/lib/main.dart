eb93a2ce30c8ac8dbb7dd5556e28a7202ae6a72b
nahomsof <nahomsileshi21@gmail.com>
2024-04-28 21:36:53 +0300

    commit

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,25 +1,17 @@
-import 'dart:js';
-
 import 'package:ecommerce_app/Pages/cart_page.dart';
 import 'package:ecommerce_app/Pages/intropage.dart';
-import 'package:ecommerce_app/Pages/setting_page.dart';
+
 import 'package:ecommerce_app/Pages/shop_pages.dart';
 import 'package:ecommerce_app/models/shop.dart';
-import 'package:ecommerce_app/themes/light_mode.dart';
-import 'package:ecommerce_app/themes/theme_provider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-    ChangeNotifierProvider(
-      create: (context) => Shop(),
-    )
-  ]));
+  runApp(ChangeNotifierProvider(
+    create: (context) => Shop(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -30,12 +22,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const IntroPages(),
-      theme: Provider.of(context),
       routes: {
         '/intropage': (context) => const IntroPages(),
         '/shop_pages': (context) => const ShopPage(),
         '/cart_page': (context) => const CartPage(),
-        '/setting_page': (context) => const MySetting(),
       },
     );
   }
