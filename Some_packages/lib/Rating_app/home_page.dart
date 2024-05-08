6d6849cecf846b3e5d569f82a99c885a05288d69
nahomsof <nahomsileshi21@gmail.com>
2024-05-08 14:15:28 +0300

    Commit

--- Some_packages/lib/Rating_app/home_page.dart
+++ Some_packages/lib/Rating_app/home_page.dart
@@ -14,15 +14,41 @@ class _MyHomepageState extends State<MyHomepage> {
     return Scaffold(
       body: Center(
         child: RatingBar.builder(
-            itemCount: 7,
+            itemCount: 5,
             allowHalfRating: true,
             initialRating: 2,
-            itemPadding: const EdgeInsets.symmetric(horizontal: 5),
+            minRating: 1,
+            itemPadding: const EdgeInsets.symmetric(horizontal: 7),
             direction: Axis.horizontal,
-            itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: Colors.red,
-                ),
+            itemBuilder: (context, index) {
+              switch (index) {
+                case 0:
+                  return const Icon(Icons.sentiment_very_dissatisfied,
+                      color: Colors.red);
+                case 1:
+                  return const Icon(
+                    Icons.sentiment_dissatisfied,
+                    color: Colors.redAccent,
+                  );
+                case 2:
+                  return const Icon(
+                    Icons.sentiment_neutral,
+                    color: Colors.amber,
+                  );
+                case 3:
+                  return const Icon(
+                    Icons.sentiment_satisfied,
+                    color: Colors.greenAccent,
+                  );
+                case 4:
+                  return const Icon(
+                    Icons.sentiment_very_satisfied,
+                    color: Colors.green,
+                  );
+                default:
+                  return Text("");
+              }
+            },
             onRatingUpdate: (value) {
               ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(content: Text("Giving rating is $value")));
