b6643f7e00f5bde39ac84908b9d3078d9459f79c
nahomsof <nahomsileshi21@gmail.com>
2024-05-11 01:14:25 +0300

    Commti

--- habit_truck/lib/pages/home_page.dart
+++ habit_truck/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_truck/component/drawer.dart';
+import 'package:habit_truck/database/habit_database.dart';
 import 'package:habit_truck/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -11,20 +13,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //text conroller
+  final TextEditingController textController = TextEditingController();
+  //create new habit
+  void createnewhabit() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+                decoration: InputDecoration(hintText: "Creat a new habit"),
+              ),
+              actions: [
+                MaterialButton(onPressed: () {
+                  String newhabitname = textController.text;
+                  //save to db
+                  context.read<HabitDatabase>().addHabit(newhabitname);
+                  //pop box
+                  Navigator.pop(context);
+                  //clear controller
+                  textController.clear();
+                })
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) =>
-                Provider.of<ThemeProvider>(context, listen: false)
-                    .toggleTheme(),
-          ),
-        ),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createnewhabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(Icons.add),
       ),
     );
   }
