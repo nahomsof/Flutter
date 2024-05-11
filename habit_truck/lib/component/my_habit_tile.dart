a3bcf725de4284b5f9f963142f84f33ae1a64f6c
nahomsof <nahomsileshi21@gmail.com>
2024-05-11 15:10:23 +0300

    Commi

--- habit_truck/lib/component/my_habit_tile.dart
+++ habit_truck/lib/component/my_habit_tile.dart
@@ -17,47 +17,54 @@ class MyHabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const StretchMotion(),
-        children: [
-          SlidableAction(
-            onPressed: edithabit,
-            backgroundColor: Colors.grey.shade800,
-            icon: Icons.settings,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          SlidableAction(
-            onPressed: deleteHabit,
-            backgroundColor: Colors.red,
-            icon: Icons.delete,
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ],
-      ),
-      child: GestureDetector(
-        onTap: () {
-          if (onChanged != null) {
-            //toggle completion status
-            onChanged!(!inComplite);
-          }
-        },
-        child: Container(
-            decoration: BoxDecoration(
-              color: inComplite
-                  ? Colors.green
-                  : Theme.of(context).colorScheme.secondary,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: edithabit,
+              backgroundColor: Colors.grey.shade800,
+              icon: Icons.settings,
+              borderRadius: BorderRadius.circular(8),
             ),
-            padding: const EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-            child: ListTile(
-              title: Text(text),
-              leading: Checkbox(
-                activeColor: Colors.green,
-                value: inComplite,
-                onChanged: onChanged,
-              ),
-            )),
+            SlidableAction(
+              onPressed: deleteHabit,
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ],
+        ),
+        child: GestureDetector(
+          onTap: () {
+            if (onChanged != null) {
+              //toggle completion status
+              onChanged!(!inComplite);
+            }
+          },
+          child: Container(
+              decoration: BoxDecoration(
+                  color: inComplite
+                      ? Colors.green
+                      : Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(8)),
+              padding: const EdgeInsets.all(12),
+              child: ListTile(
+                title: Text(text,
+                    style: TextStyle(
+                      color: inComplite
+                          ? Colors.white
+                          : Theme.of(context).colorScheme.inversePrimary,
+                    )),
+                leading: Checkbox(
+                  activeColor: Colors.green,
+                  value: inComplite,
+                  onChanged: onChanged,
+                ),
+              )),
+        ),
       ),
     );
   }
