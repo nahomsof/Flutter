565220715e8c8ddc7ba9ca5f0a35b0e53f27d9f1
nahomsof <nahomsileshi21@gmail.com>
2024-05-16 10:16:17 +0300

    commit

--- habit_truck/lib/component/my_heat_map.dart
+++ habit_truck/lib/component/my_heat_map.dart
@@ -10,7 +10,7 @@ class MyHeatMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return HeatMap(
         startDate: startDate,
-        datasets: datasets,
+        datasets: datasts,
         colorMode: ColorMode.color,
         defaultColor: Theme.of(context).colorScheme.secondary,
         endDate: DateTime.now(),
