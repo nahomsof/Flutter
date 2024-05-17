0ca2df39dcbdf089011142841050b0e870ceebb5
nahomsof <nahomsileshi21@gmail.com>
2024-05-18 01:01:46 +0300

    commit

--- authentication/lib/main.dart
+++ authentication/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:authentication/firebase_options.dart';
 import 'package:authentication/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
