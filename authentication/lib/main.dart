da31ffd029d9c3aa8ecd189cf0056e0a637e2be5
nahomsof <nahomsileshi21@gmail.com>
2024-05-17 13:38:01 +0300

    Commit

--- authentication/lib/main.dart
+++ authentication/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:authentication/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: LoginPage(),
+    );
   }
 }
--- authentication/lib/pages/login_page.dart
+++ authentication/lib/pages/login_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //logo
+          Icon(
+            Icons.lock,
+            size: 100,
+          )
+          //welcome back, you have been missed!
+
+          //username textfield
+
+          //password textfield
+
+          //forgot password?
+
+          //sign in button
+
+          //or continut with
+
+          //google + apple sign in buttons
+
+          //not a  member?  register now
+        ],
+      ),
     );
   }
 }
