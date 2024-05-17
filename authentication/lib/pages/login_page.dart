7eb390344ba84d6beda596e78c4143094f5189c5
nahomsof <nahomsileshi21@gmail.com>
2024-05-17 13:50:23 +0300

    Commit

--- authentication/lib/pages/login_page.dart
+++ authentication/lib/pages/login_page.dart
@@ -7,29 +7,50 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          //logo
-          Icon(
-            Icons.lock,
-            size: 100,
-          )
-          //welcome back, you have been missed!
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              //logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              //welcome back, you have been missed!
+              Text(
+                "Welcome back, you've been missed!",
+                style: TextStyle(color: Colors.grey[700], fontSize: 16),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              //username textfield
+              TextField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white))),
+              )
 
-          //username textfield
+              //password textfield
 
-          //password textfield
+              //forgot password?
 
-          //forgot password?
+              //sign in button
 
-          //sign in button
+              //or continut with
 
-          //or continut with
+              //google + apple sign in buttons
 
-          //google + apple sign in buttons
-
-          //not a  member?  register now
-        ],
+              //not a  member?  register now
+            ],
+          ),
+        ),
       ),
     );
   }
