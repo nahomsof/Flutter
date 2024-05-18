a33c16f577668d8bb08f2d8a851430e280cf410b
nahomsof <nahomsileshi21@gmail.com>
2024-05-18 20:17:53 +0300

    Commti

--- authentication/lib/main.dart
+++ authentication/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:authentication/firebase_options.dart';
 import 'package:authentication/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
