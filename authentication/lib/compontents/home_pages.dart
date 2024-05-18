80045bf07155a7893060d6900c64ec99878303d6
nahomsof <nahomsileshi21@gmail.com>
2024-05-18 22:21:55 +0300

    commit

--- authentication/lib/compontents/home_pages.dart
+++ authentication/lib/compontents/home_pages.dart
@@ -2,7 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final user = FirebaseAuth.instance.currentUser!;
+  HomePage({super.key});
   //signuser out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
