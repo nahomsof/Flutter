7df7b8ed81f75b711c2f5df60bd5d7fb2fac6bdf
nahomsof <nahomsileshi21@gmail.com>
2024-05-20 23:00:13 +0300

    commit

--- online_database/lib/main.dart
+++ online_database/lib/main.dart
@@ -1,21 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:online_database/firebase_options.dart';
+import 'package:online_database/pages/home_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold
-
-      ),
-     
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
-
--- online_database/lib/services/firestore.dart
+++ online_database/lib/services/firestore.dart
@@ -1,1 +1,19 @@
-class
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  //get collection of notes
+  final CollectionReference notes =
+      FirebaseFirestore.instance.collection("notes");
+
+  //Create
+  Future<void> addNotes(String note) {
+    return notes.add({
+      'note': note,
+      'timestamp': Timestamp.now(),
+    });
+  }
+
+  //Read
+
+  //Update
+}
