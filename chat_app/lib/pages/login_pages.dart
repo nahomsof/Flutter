12012bd124cb6b0fe43afa13548752646594f62a
nahomsof <nahomsileshi21@gmail.com>
2024-05-25 10:06:58 +0300

    commit

--- chat_app/lib/pages/login_pages.dart
+++ chat_app/lib/pages/login_pages.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
 
   @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: Column(
+        children: [
+          _headerText(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _headerText() {
+    return SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: const Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hi, Welcome Back!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+            ),
+            Text(
+              "Hello again, you've been missed",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+            )
+          ],
+        ));
   }
 }
