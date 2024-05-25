c78c02d7e16338cfded77852f1913f702bfde5ee
nahomsof <nahomsileshi21@gmail.com>
2024-05-25 11:55:46 +0300

    commit

--- chat_app/lib/pages/login_pages.dart
+++ chat_app/lib/pages/login_pages.dart
@@ -2,7 +2,10 @@ import 'package:chat_app/componenet/my_textfiled.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  // email and pw text
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +32,24 @@ class LoginPage extends StatelessWidget {
           //email textefield
           MyTextField(
             hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
           ),
           const SizedBox(
             height: 10,
           ),
           //pw textfield
-          MyTextField(hintText: "Password")
+          MyTextField(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwController,
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+
+          // login button
+
           //rgister now
         ],
       ),
