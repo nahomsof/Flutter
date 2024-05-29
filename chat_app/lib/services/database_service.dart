691a75b57866d4679d0b8b2312135c6dde196ae1
nahomsof <nahomsileshi21@gmail.com>
2024-05-29 09:03:11 +0300

    commit

--- chat_app/lib/services/database_service.dart
+++ chat_app/lib/services/database_service.dart
@@ -1,13 +1,23 @@
+import 'package:chat_app/model/chat.dart';
 import 'package:chat_app/model/user_profile.dart';
+import 'package:chat_app/services/auth_service.dart';
+import 'package:chat_app/utils.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:collection/collection.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseService {
+  final GetIt _getIt = GetIt.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   CollectionReference? _usersCollection;
+   CollectionReference? _chatsCollection;
+  
+
+  late AuthService _authService;
 
   DatabaseService() {
+    _authService = _getIt.get<AuthService>();
     _setupCollectionRefernces();
   }
   void _setupCollectionRefernces() {
@@ -18,9 +28,23 @@ class DatabaseService {
               ),
               toFirestore: (userProfile, _) => userProfile.toJson(),
             );
+           _chatsCollection = _firebaseFirestore.collection("chats").withConverter<Chat>(
+              fromFirestore: (snapshots, _) => Chat.fromJson(snapshots.data()!),
+               toFirestore: (chat, _) => chat.toJson());
   }
 
   Future<void> createUserProfile({required UserProfile userProfile}) async {
     await _usersCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>> getUserProfiles() {
+    return _usersCollection
+        ?.where("uid", isNotEqualTo: _authService.user!.uid)
+        .snapshots() as Stream<QuerySnapshot<UserProfile>>;
+  }
+
+  Future<bool> checkChatExists(String uid1, String uid2) async {
+    String chatID = generateChatID(uid1: uid1, uid2: uid2);
+    final result
   }
 }
