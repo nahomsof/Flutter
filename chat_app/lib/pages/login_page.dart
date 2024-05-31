0c7acab43c0ade4e2eea4dab664a7869e4212622
nahomsof <nahomsileshi21@gmail.com>
2024-05-31 13:17:21 +0300

    commie

--- chat_app/lib/pages/login_page.dart
+++ chat_app/lib/pages/login_page.dart
@@ -4,7 +4,6 @@ import 'package:chat_app/services/auth_service.dart';
 import 'package:chat_app/services/navigation_service.dart';
 import 'package:chat_app/widgets/custom_form_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 
 class LoginPage extends StatefulWidget {
