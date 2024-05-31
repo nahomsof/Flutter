af47bd73b43658320fe5d2f8a8efa44920cf5241
nahomsof <nahomsileshi21@gmail.com>
2024-05-31 13:28:28 +0300

    commit

--- provider/lib/Provider/home_page.dart
+++ provider/lib/Provider/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:_provider/counter_model.dart';
+import 'package:_provider/Provider/counter_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
