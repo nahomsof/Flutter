93d0cac0d427c4ba322eb3495f41da5a8385db23
nahomsof <nahomsileshi21@gmail.com>
2024-06-10 19:31:41 +0300

    commit

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:chat_app/services/auth_service.dart';
 import 'package:chat_app/services/navigation_service.dart';
 import 'package:chat_app/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await setup();
@@ -17,16 +19,30 @@ Future<void> setup() async {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  final GetIt _getIt = GetIt.instance;
+  late NavigationService _navigationService;
+  late AuthService _authService;
+  MyApp({super.key}) {
+    _navigationService = _getIt.get<NavigationService>();
+    _authService = _getIt.get<AuthService>();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigationService.navigatorKey,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: const LoginPage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+        //textTheme: GoogleFonts.monntserraTextTheme(),
+      ),
+      initialRoute: _authService.user != null ? "/home" : "/login",
+      routes: _navigationService.routes,
     );
   }
 }
