9c0d5ab82fd54916732ebc4a50521291bb4e9a00
nahomsof <nahomsileshi21@gmail.com>
2024-06-11 15:51:35 +0300

    commit

---
+++ chat_app/lib/pages/login_pages.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: Column(
+        children: [
+          _headerText(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _headerText() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Hi, Welcome Back!",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
+          Text(
+            "Hello again, you've been missed",
+            style: TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w500, color: Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+}
