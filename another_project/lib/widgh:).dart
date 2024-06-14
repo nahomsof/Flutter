36488872118adde90b089e7959a78e43cc9e4630
nahomsof <nahomsileshi21@gmail.com>
2024-06-14 19:03:21 +0300

    commit

--- another_project/lib/widgh:).dart
+++ another_project/lib/widgh:).dart
@@ -1,39 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-void main() => runApp(App0501());
+void main() {
+  runApp(Currentap());
+}
 
-class App0501 extends StatelessWidget {
+class Currentap extends StatelessWidget {
+  Currentap({super.key});
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: _MyHomepage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State {
-  String _pressedOrNot = "You haven't pressed the button.";
-  void _changeText() {
-    setState(_getNewText);
-  }
-
-  void _getNewText() {
-    _pressedOrNot = "You've pressed the button.";
-  }
-
+class _MyHomepage extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text(
-        _pressedOrNot,
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _changeText,
+    return Material(
+      color: Colors.grey[400],
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: buildColumn(context),
       ),
     );
   }
+}
+
+Widget buildTitleText() {
+  return Text(
+    "My Pet Shop",
+    textScaleFactor: 3.0,
+    textAlign: TextAlign.center,
+  );
+}
+
+Widget buildRoundedBox(String label, {double height = 88.0}) {
+  return Container(
+    height: height,
+    width: 88.0,
+    alignment: Alignment(0.0, 0.0),
+    decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10.0)),
+    child: Text(
+      label,
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget buildColumn(BuildContext context) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: <Widget>[
+      buildTitleText(),
+      SizedBox(
+        height: 20,
+      ),
+      buildRoundedBox("Sale Today", height: 150.0)
+    ],
+  );
 }
