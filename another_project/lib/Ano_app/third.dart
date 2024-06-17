9cb75542ee4a493e0af9478c4e01262eb6fb2126
nahomsof <nahomsileshi21@gmail.com>
2024-06-18 00:35:06 +0300

    commit

--- another_project/lib/Ano_app/third.dart
+++ another_project/lib/Ano_app/third.dart
@@ -28,35 +28,39 @@ String _messageTour = "$_youAre Not $_compatible";
 class _HomePage2State extends State<HomePage2> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Are yo ucompative with Doris"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(12),
-        child: Column(
-          children: <Widget>[
-            _buildFlutterlove(),
-            Row(
-              children: [_button(), _text()],
-            )
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Are yo ucompative with Doris"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            children: <Widget>[
+              _buildFlutterlove(),
+              Row(
+                children: [_button(), _text()],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildFlutterlove() {
-    return Row(
+    return Column(
       children: [
         Text("On scale of 1 to 10, "
             "How much do you love developing flutter apps"),
         Slider(
-            min: 0,
-            max: 9,
-            divisions: 10,
-            value: _loveFlutterSliderValue,
-            onChanged: _updateFlutterlove)
+          min: 0,
+          max: 9,
+          divisions: 10,
+          value: _loveFlutterSliderValue,
+          onChanged: _updateFlutterlove,
+          label: '${_loveFlutterSliderValue.toInt()}',
+        )
       ],
     );
   }
@@ -71,7 +75,7 @@ class _HomePage2State extends State<HomePage2> {
 
   Widget _text() {
     return Row(
-      children: [
+      children: <Widget>[
         Text(
           _messageTour,
           textAlign: TextAlign.center,
